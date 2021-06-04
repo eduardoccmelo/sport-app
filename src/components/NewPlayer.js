56534a60b73b8b652e11b16f9ff2ab5a305a0755
Eduardo Melo <eduardoccmelo@gmail.com>
2021-06-04 17:01:09 +0200

    console log input value and clear the input after submit

--- src/components/NewPlayer.js
+++ src/components/NewPlayer.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 export default function NewPlayer() {
+  const [newPlayer, setNewPlayer] = useState();
+
+  function handleAddPlayerClick(e) {
+    e.preventDefault();
+    let inputValue = e.target.addPlayer.value;
+    setNewPlayer(inputValue);
+    e.target.addPlayer.value = "";
+  }
+
+  console.log(newPlayer);
+
   return (
-    <div className="newPlayer">
+    <form className="newPlayer" onSubmit={handleAddPlayerClick}>
       <label>
-        <input type="text" placeholder="New Player"></input>
+        <input
+          type="text"
+          name="addPlayer"
+          id="addPlayer"
+          placeholder="New Player"
+        ></input>
       </label>
-      <button className="newPlayerButton" type="button">
+      <button className="newPlayerButton" type="submit">
         Add
       </button>
-    </div>
+    </form>
   );
 }
